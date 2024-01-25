5900a21f6405614158af3cb8b11f469a4ea6c328
sw-codekit <sw.jang@codekit.im>
2024-01-25 18:54:43 +0900

    Feat: Add '시작하기' button action

--- js/main.js
+++ js/main.js
@@ -5,24 +5,37 @@ const mainEl = document.querySelector(".main")
 const menuEl = document.querySelector(".header__menu")
 const logoEl = document.querySelector(".header__logo")
 const formEl = document.querySelector(".form-box__form")
+const contactButton = document.querySelector(
+  ".lead-contact__text-box .contact-button"
+)
+
+const toggleMenu = (text) => {
+  switch (text) {
+    case "about":
+      mainEl.classList.add(SHOWING_ABOUT_CLASS_NAME)
+      mainEl.classList.remove(SHOWING_CONTACT_CLASS_NAME)
+      break
+    default:
+      mainEl.classList.remove(SHOWING_ABOUT_CLASS_NAME)
+      mainEl.classList.add(SHOWING_CONTACT_CLASS_NAME)
+  }
+}
 
 menuEl.addEventListener("click", (e) => {
   const linkEl = e.target
 
   const textValue = linkEl.textContent
 
-  if (textValue === "about") {
-    mainEl.classList.add(SHOWING_ABOUT_CLASS_NAME)
-    mainEl.classList.remove(SHOWING_CONTACT_CLASS_NAME)
-  } else {
-    mainEl.classList.remove(SHOWING_ABOUT_CLASS_NAME)
-    mainEl.classList.add(SHOWING_CONTACT_CLASS_NAME)
-  }
+  toggleMenu(textValue)
 })
 
 logoEl.addEventListener("click", () => {
   mainEl.classList.add(SHOWING_ABOUT_CLASS_NAME)
   mainEl.classList.remove(SHOWING_CONTACT_CLASS_NAME)
+})
+
+contactButton.addEventListener("click", () => {
+  toggleMenu()
 })
 
 formEl.addEventListener("submit", (e) => {
