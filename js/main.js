6aa8d3c3ec90b56def7692bff85bc4e65ae4317c
sw-codekit <sw.jang@codekit.im>
2024-01-29 21:50:55 +0900

    Feat: contact form - saved spread sheet

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+const EMAIL_URL =
+  "https://script.google.com/macros/s/AKfycbwBy8tu71VY6YII5zeAcftUn0gIoIfy9JQs0J_Yva24RZ9KVuSw62qUXTinihpe3C3s/exec"
+
 const SHOWING_ABOUT_CLASS_NAME = "main--about"
 const SHOWING_CONTACT_CLASS_NAME = "main--contact"
 
@@ -8,6 +11,7 @@ const formEl = document.querySelector(".form-box__form")
 const contactButton = document.querySelector(
   ".lead-contact__text-box .contact-button"
 )
+const overlayEl = document.querySelector(".overlay")
 
 const toggleMenu = (text) => {
   switch (text) {
@@ -38,6 +42,55 @@ contactButton.addEventListener("click", () => {
   toggleMenu()
 })
 
+const formObj = {
+  "team-name": "조직명은 필수 항목입니다.",
+  "manager-name": "담당자명은 필수 항목입니다.",
+  "phone-number": "휴대폰번호는 필수 항목입니다.",
+  email: "이메일은 필수 항목입니다.",
+  "company-contact": null,
+  "existing-services": null,
+  "reference-services": null,
+}
+
 formEl.addEventListener("submit", (e) => {
   e.preventDefault()
+
+  const formData = new FormData(e.currentTarget)
+  const transFormData = {}
+
+  for (const key of formData.keys()) {
+    if (formObj[key] && !formData.get(key)) {
+      return alert(formObj[key])
+    }
+
+    transFormData[key] = formData.get(key)
+  }
+
+  overlayEl.classList.add("overlay--active")
+
+  const encoded = Object.keys(transFormData)
+    .map(
+      (key) =>
+        encodeURIComponent(key) + "=" + encodeURIComponent(transFormData[key])
+    )
+    .join("&")
+
+  fetch(EMAIL_URL, {
+    redirect: "follow",
+    mode: "no-cors",
+    method: "POST",
+    body: encoded,
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+  })
+    .then((res) => {
+      alert("정상적으로 요청이 전송되었습니다.")
+    })
+    .catch((err) => {
+      alert("오류가 발생했습니다. 잠시 후 다시 시도해 주세요.")
+    })
+    .finally(() => {
+      overlayEl.classList.remove("overlay--active")
+    })
 })
